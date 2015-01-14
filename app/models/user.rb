e57eb60ed85b6d93ddf795245e097b0716887950
parminder <parminder@ezzie.in>
2015-01-14 11:14:14 +0530

    user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,  :authentication_keys => [:login]
   validates :username,  :uniqueness => { :case_sensitive => false } 
-  
+  validates :school_id, :presence => true
   # before_create :create_login
 
   # def create_login
